a2563f8949d24e156af12a6ee1aa86ca68524b30
Can Sözverdi <sozverdican@gmail.com>
2026-04-15 16:58:09 +0300

    Takipçi sayacı eklendi

---
+++ followersCount/followers_count.js
@@ -0,0 +1,20 @@
+let count=0; //Inıtialize count to 0
+
+function increaseCount(){
+    count++;
+    displayCount();
+    checkCountValue();
+}
+
+function displayCount(){
+    document.getElementById('countDisplay').innerHTML=count; //show count on HTML
+}
+
+function checkCountValue(){
+    if(count === 10){
+        alert("Instagram gonderiniz 10 takipci kazandi! Tebrikler!");
+    }
+    else if(count === 20){
+        alert("Instagram gonderiniz 20 takipci kazandi! Devam edin!");
+    }
+}
